4b36925da7e9a51dd9b3c98f375f4653c148fbc5
c00lerxo <szymonkuleczka98@gmail.com>
2020-01-21 22:43:15 +0100

    add labs 2 task 2

--- labs2.r
+++ labs2.r
@@ -1,5 +1,8 @@
 install.packages("magrittr", repos = "http://cran.us.r-project.org")
+install.packages("moments")
+
 library(magrittr)
+library(moments)
 
 
 # Task 2.1
@@ -123,4 +126,105 @@ BarChartBrokenByWomenAndMen(men_empirical_distribution, women_empirical_distribu
 # survey's participants were mainly women (68 vs 32).
 # Men seem to be more satisfied with president's activity, and also less often gave 'i have no opinion' answer.
 # (needs to be checked, as men sample size is much smaller than women sample size)
+
+
+# Task 2.1
+###
+# 200 randomly selected 5-second time periods of work of atelephone exchange
+# were examined. The number of calls was recorded. The results are contained
+# in the file telephone_exchange.RData.
+# Which type is this variable? What are its possible values?
+
+# This variable is a quantitative, discrete random variable.
+# Set of values is {0, 1, 2, 3, 4, 5}
+
+# Import the data from the file telephone_exchange.RData.
+
+load('~/uam/stats/telephone_exchange.rdata')
+
+# Present the empirical distribution of the number of calls in the form of a table.
+
+NumberOfCallsEmpiricalDistribution <- function() {
+  data.frame(cbind(number_of_calls = table(telephone_exchange$number),
+                   frequency = prop.table(table(telephone_exchange$number))))
+}
+
+NumberOfCallsEmpiricalDistribution() %>%
+  print()
+
+# Illustrate the empirical distribution of the number of calls using a bar chart and pie chart.
+
+BarChartForNumberOfCalls <- function(empirical_distribution, probability = FALSE) {
+  if (probability == TRUE) {
+    barplot(empirical_distribution$number_of_calls,
+            names.arg = row.names(empirical_distribution),
+            xlab = "Number of calls",
+            ylab = "Probability",
+            main = "Empirical distribution of number of calls",
+            col = GetRandomColorsVector(length(empirical_distribution$frequency)))
+  }
+  else {
+    barplot(empirical_distribution$frequency,
+            names.arg = row.names(empirical_distribution),
+            xlab = "Number of calls",
+            ylab = "Probability",
+            main = "Empirical distribution of number of calls",
+            col = GetRandomColorsVector(length(empirical_distribution$frequency)))
+  }
+}
+
+PieChartForNumberOfCalls <- function(empirical_distribution) {
+  slices <- empirical_distribution$number_of_calls
+  labels <- row.names(empirical_distribution)
+  
+  pie(slices, labels, col = GetRandomColorsVector(length(empirical_distribution$frequency)))
+}
+
+NumberOfCallsEmpiricalDistribution() %>%
+  BarChartForNumberOfCalls()
+
+NumberOfCallsEmpiricalDistribution() %>%
+  BarChartForNumberOfCalls(probability = TRUE)
+
+NumberOfCallsEmpiricalDistribution() %>%
+  PieChartForNumberOfCalls
+
+# Calculate the mean of the number of calls,
+# the median of the number of calls,
+# the standard deviation of the number of calls,
+# and the coefficient of variation of the number of calls.
+
+# Mean
+mean <- mean(telephone_exchange$number) %>%
+  print()
+
+# Median
+median(telephone_exchange$number) %>%
+  print()
+
+# Standard deviation
+sd <-sd(telephone_exchange$number) %>%
+  print()
+
+cv <- function(sd, mean) {
+  (sd / mean) * 100
+}
+
+# Coefficient of variation
+cv(sd, mean) %>%
+  print()
+
+
+# Interpret the above tabular, graphical and numerical results.
+
+# In the sample, most often there was only 1 call in time period of 5 seconds (33.5%),
+# although it's not a majority.
+# Median (2) and average (1.74) are quite similar, which suggests that a central tendency
+# in the sample is 2 calls per 5 seconds.
+# Standard deviation (1.28086) seems to be quite small at the first glance,
+# but actually it's not, as our data values are in the set {0, 1, 2, 3, 4, 5}.
+# It suggests that our data is dispersed.
+# Also, the coefficient of variation (73%) has a big value, which also gives us
+# a hint about data being widely spread-out.
+
 
